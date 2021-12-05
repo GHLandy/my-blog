43ec4deed967b9378cdc7820f14658f58accc2d5
GHLandy <liftland953@163.com>
2021-12-05 23:31:53 +0800

    enable google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,12 +48,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: ['G-G582XR7M2S'],
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
